930e5fc75945c9d3b8a6310f3a3ee48829207f73
Cosmolocker <kaiyue1995@gmail.com>
2018-09-21 13:21:41 +1200

    Update - Title page and instruction

---
+++ Go Go Fish/Assets/AssetsFiles/InGame/ScoreScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScoreScript : MonoBehaviour {
+    Text scoreText;
+    int score = 1950;
+    int goBackTimer = 300;
+    int scoreCount = 0;
+
+    // Use this for initialization
+    void Start () {
+        scoreText = GetComponent<Text>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(GameObject.Find("FishPlayer") != null)
+        {
+            scoreCount++;
+            scoreText.text = "Year: " + score;
+
+            if (scoreCount == 60)
+            {
+                scoreCount = 0;
+                score++;
+            }
+        }
+        else
+        {
+            scoreText.text = "Year: " + score;
+            scoreText.text += "\n\n            FINAL YEAR (SCORE): " + score;
+            scoreText.text += "\n                        GAME OVER";
+            scoreText.text += "\n      We go back to main (title) page in: " + goBackTimer/60;
+            goBackTimer--;
+
+            if(goBackTimer < 0)
+            {
+                SceneManager.LoadScene("MainMenu");
+            }
+        }
+    }
+}
---
+++ Go Go Fish/Assets/AssetsFiles/InGame/SeaweedScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeaweedScript : MonoBehaviour {
+    int speed = 10;
+    int iRandom = 0;
+    int iRandomScale = 0;
+
+    // Use this for initialization
+    void Start ()
+    {
+        speed = (int)(Random.value * 10) + 1;
+        iRandom = (int)(Random.value * 9);
+        iRandomScale = (int)(Random.value * 3);
+
+        transform.position = new Vector3(12f, (iRandom - 4) * 1.1f, 0f);
+        transform.localScale += new Vector3(iRandomScale * 1.0f, iRandomScale * 1.0f, 0);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        moveSeaweed();
+    }
+
+    void moveSeaweed()
+    {
+        transform.position += transform.right * -speed * Time.deltaTime;
+    }
+
+    void checkDestroy()
+    {
+        if(transform.position.x <= -12.0f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
